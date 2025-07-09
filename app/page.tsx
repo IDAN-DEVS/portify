0a2ec7a77e7b4127aee4856432fe3ceab7288ec0
JC-Coder <josephchimezie2003@gmail.com>
2025-07-10 00:40:23 +0100

    chore: uncomment code

--- app/page.tsx
+++ app/page.tsx
@@ -7,22 +7,22 @@ import ShadowLayout from "../templates/shadow/page";
 import LandingPage from "./landing/page";
 
 export default function Home() {
-  // // Check if we're in demo mode (environment variable or query parameter)
-  // const isDemoMode = process.env.NEXT_PUBLIC_DEMO_MODE === "true";
+  // Check if we're in demo mode (environment variable or query parameter)
+  const isDemoMode = process.env.NEXT_PUBLIC_DEMO_MODE === "true";
 
-  // // If demo mode is disabled, show the configured user template
-  // if (!isDemoMode) {
-  //   switch (AppSettingData.template) {
-  //     case AVAILABLE_TEMPLATES.BASE:
-  //       return <BaseTemplate />;
-  //     case AVAILABLE_TEMPLATES.NEON:
-  //       return <NeonTemplate />;
-  //     case AVAILABLE_TEMPLATES.SHADOW:
-  //       return <ShadowLayout />;
-  //     default:
-  //       return <BaseTemplate />;
-  //   }
-  // }
+  // If demo mode is disabled, show the configured user template
+  if (!isDemoMode) {
+    switch (AppSettingData.template) {
+      case AVAILABLE_TEMPLATES.BASE:
+        return <BaseTemplate />;
+      case AVAILABLE_TEMPLATES.NEON:
+        return <NeonTemplate />;
+      case AVAILABLE_TEMPLATES.SHADOW:
+        return <ShadowLayout />;
+      default:
+        return <BaseTemplate />;
+    }
+  }
 
   // Default to landing page in demo mode
   return <LandingPage />;
